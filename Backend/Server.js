3ec00fc2cc40e8a262702da611770c85d07170f4
ADHARSH KUMAR SINGH <singh.adarsh2003@gmail.com>
2026-03-24 23:19:33 +0530

    chore: update server formatting

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./Config/db");
 const todoRoutes = require("./Routes/todoRoutes");
